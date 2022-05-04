a14bf90cc04001457db4b9258f5ba7a644d0af3f
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-05 01:26:50 +0300

    Input and output Libraries in Java

--- competitive_programming/Basics/InputOutput.java
+++ competitive_programming/Basics/InputOutput.java
@@ -7,5 +7,6 @@ import java.util.Scanner;
 class InputOUt{
     public static void main(String[] args){
         System.out.println("This is an output");
+        System.out.print("Thanks for trying this out put in Java");
     }
 }
