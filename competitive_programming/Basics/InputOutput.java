60c659e11fb73fa36755822c24e0cbc04fee6b18
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-04 11:26:51 +0300

    Java output

---
+++ competitive_programming/Basics/InputOutput.java
@@ -0,0 +1,11 @@
+/**
+    This program explains the Output and the input in java programming language
+    Input Libraries
+    - 
+ */
+import java.util.Scanner;
+class InputOUt{
+    public static void main(String[] args){
+        System.out.println("This is an output");
+    }
+}
