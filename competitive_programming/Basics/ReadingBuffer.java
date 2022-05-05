99832a3c4967691e3f7c2ce1065024e39d468c92
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-05 20:15:32 +0300

    Using the BufferedReader library for the input stream

--- competitive_programming/Basics/ReadingBuffer.java
+++ competitive_programming/Basics/ReadingBuffer.java
@@ -1,8 +1,13 @@
-import java.io;
+import java.io.*;
 class ReadingBuffer{
     public static void main(String[] args)throws IOException{
         char c;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        System.out.println("Enter characters, 'q' to quit: ");
+        // Read characters.
+        do{
+            c = (char) br.read();
+            System.out.println(c);
+        }while(c != 'q');
     }
 }
