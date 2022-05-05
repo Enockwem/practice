aa7f06a60a097940d9d412ca53fb09ac0b398ac1
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-05 22:52:37 +0300

    Reading a string in ReadingBuffer.java

--- competitive_programming/Basics/ReadingBuffer.java
+++ competitive_programming/Basics/ReadingBuffer.java
@@ -2,6 +2,7 @@ import java.io.*;
 class ReadingBuffer{
     public static void main(String[] args)throws IOException{
         char c;
+        String k;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter characters, 'q' to quit: ");
         // Read characters.
@@ -9,5 +10,8 @@ class ReadingBuffer{
             c = (char) br.read();
             System.out.println(c);
         }while(c != 'q');
+        System.out.println("Enter the last string: ");
+        k = br.readLine();
+        System.out.println(k);
     }
 }
