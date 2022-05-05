8506b973d1139fbfdc08d3ec7154615dffc53f94
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-05 20:07:51 +0300

    Using the stream input and output in java

--- competitive_programming/Basics/InputOutput.java
+++ competitive_programming/Basics/InputOutput.java
@@ -1,12 +1,17 @@
+import java.io; // This library contains the stream class for the input/output
 /**
     This program explains the Output and the input in java programming language
     Input Libraries
-    - 
+    - Streams - This is an abstraction that either consumes or produces information.
+    - Byte stream and character streams.
  */
 import java.util.Scanner;
 class InputOUt{
     public static void main(String[] args){
         System.out.println("This is an output");
         System.out.print("Thanks for trying this out put in Java");
-    }
+
+        // Input 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+;    }
 }
---
+++ competitive_programming/Basics/ReadingBuffer.java
@@ -0,0 +1,8 @@
+import java.io;
+class ReadingBuffer{
+    public static void main(String[] args)throws IOException{
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
