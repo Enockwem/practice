eedac42c6f1eb38c0acb44fe73d23493ae79e2f8
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-06 11:10:06 +0300

    Moved all the input file examples to the folder called 'inputExampes' and created a folder called output that contains output examples

---
+++ competitive_programming/Basics/output/WriteDemo.java
@@ -0,0 +1,10 @@
+// System.out is a byte stream for the PrintStream
+// Though the most common output method is the 'println' and the 'print'
+class WriteDemo{
+    public static void main(String[] args){
+        int a;
+        a = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
