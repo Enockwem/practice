35e361f009c1462fa528e0611000033f64e5fc69
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-06 11:26:12 +0300

    Created the methods for each Letter initial in the letter alphabet in the LetterInitials.java file

---
+++ competitive_programming/Basics/output/LetterInitials.java
@@ -0,0 +1,85 @@
+/**
+    Since, there are 26 letters, therefore, there are gonna be 26 methods in this class.
+    These methods are gonna be able to create letter initials using the esterics
+ */
+class LetterInitials{
+    // Letter A
+    void initial_a(){
+        System.out.println()
+    }
+
+    // Letter B
+    void initial_b(){}
+
+    // Letter C
+    void initial_c(){}
+
+    // Letter D
+    void initial_d(){}
+
+    // Letter E
+    void initial_e(){}
+
+    // Letter F
+    void initial_f(){}
+
+    // Letter G
+    void initial_g(){}
+
+    // Letter H
+    void initial_h(){}
+    
+    // Letter I
+    void initial_i(){}
+
+    // Letter J
+    void initial_j(){}
+
+    // Letter K
+    void initial_k(){}
+
+    // Letter L
+    void initial_l(){}
+
+    // Letter M
+    void initial_m(){}
+
+    // Letter N
+    void initial_n(){}
+
+    // Letter O
+    void initial_o(){}
+
+    // Letter P
+    void initial_p(){}
+
+    // Letter Q
+    void initial_q(){}
+
+    // Letter R
+    void initial_r(){}
+
+    // Letter S
+    void initial_s(){}
+
+    // Letter T
+    void initial_t(){}
+
+    // Letter U
+    void initial_u(){}
+
+    // Letter V
+    void initial_v(){}
+
+    // Letter W
+    void initial_w(){}
+
+    // Letter X
+    void initial_x(){}
+
+    // Letter Y
+    void initial_y(){}
+
+    // Letter Z
+    void initial_z(){}
+}
