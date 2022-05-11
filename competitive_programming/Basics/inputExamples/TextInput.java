fc5f668009616aa77c1560e689a585434c64b4d4
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-05-11 12:07:08 +0300

    Adding the TextIO as a static class that an be used to input and out put information in java.

---
+++ competitive_programming/Basics/inputExamples/TextInput.java
@@ -0,0 +1,23 @@
+/**
+This program is implementing a simple program that calculates the area of a circle
+using the TextIO java library to get user input s
+ */
+ import java.io.*;
+ import static lang.java.Math;
+class TextInput{
+    public static void main(String[] args){
+        // Declare the variables here
+        double radius;
+        double area;
+
+        // Here, I declared and initialized the value of the 'pi'
+        double pi = 3.14;
+        // Using the TextIO to allow a user input the radius
+        TextIO.put("Enter the radius of the circle: ");
+        radius = TextIO.getInDouble();
+
+        // Now calculate the area of the circle.
+        area = pi * Math.pow(radius,2);
+        TextIO.put("The area of the circle is "+area);
+    }
+}
