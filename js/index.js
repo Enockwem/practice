d1c183672a344a0ce5cbe2afd56547d3b8c6e0af
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-12 13:45:13 +0300

    Added the description of the game in the index.js file

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ * The user should first select whether to use 'x' or to use 'o'.
+ * I am going to first customize it, so that the computer uses 'o' and the one playing against to use 'x'
+ * When the user click a given tile, it should be labeled the letter he choose.
+ * I am gonna use array operations in javascript to be able to capture the logic of the game.
+ * @author WASSWA ENOCK MALE
+ * This is a tic tac toe game.
+ */
