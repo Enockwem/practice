14527b3376b8b9f5c9b5e34996868f0e8068a32e
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-23 23:07:13 +0300

    Imported the removeOperator and thousandOperator functions

--- tip-calculator-app-main/js/App.js
+++ tip-calculator-app-main/js/App.js
@@ -1,5 +1,21 @@
+console.log("I am here")
+import { removeComma } from "./removeOperator"
+import { thousandOperator } from "./thousandOperator"
+
 const bill = document.getElementById("bill-amount")
 const numPeople = document.getElementById("num-people")
 const tipAmount = document.getElementById("tip-amount")
 const total = document.getElementById("total")
 
+
+let bill_input = ""
+bill.addEventListener('input',(event)=>{
+    bill_input = event.target.value 
+    console.log(bill_input)
+    // if(bill_input === ""){
+    //     tipAmount.innerHTML = "0.00"
+    // }else{
+    //     tipAmount.innerHTML = bill_input
+    // }
+    // event.target.value = thousandOperator(removeComma(bill_input))
+})
