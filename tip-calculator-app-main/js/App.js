64625eacd2855854c231534a1a4e7f822f687b22
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-24 11:49:04 +0300

    I have designed the emptiness of the number of people input

--- tip-calculator-app-main/js/App.js
+++ tip-calculator-app-main/js/App.js
@@ -9,6 +9,7 @@ const tip_input = document.getElementById("tipAmount")
 const people = document.getElementById("people")
 
 let bill_input = ""
+let num_input = ""
 bill.addEventListener('input',(event)=>{
     bill_input = event.target.value 
     console.log(bill_input)
@@ -26,7 +27,7 @@ bill.onblur = function(){
         const empty = document.querySelector(".empty-message")
         let p = document.createElement('p')
         p.innerHTML = "Can't be empty"
-        p.classList.add("cantBe")
+        p.classList.add("cantBebill")
         empty.append(p)
         tip_input.classList.remove("input-div")
         tip_input.classList.add("input-empty")
@@ -36,10 +37,45 @@ bill.onblur = function(){
 // When forcus is on, then, it will remove the 'can't be empty message'
 bill.onfocus = function(){
     let empty = document.querySelector(".empty-message")
-    let p = document.querySelector(".cantBe")
+    let p = document.querySelector(".cantBebill")
     if(p !== null){
         empty.removeChild(p)
         tip_input.classList.remove("input-empty")
         tip_input.classList.add("input-div")
+    }
+}
+
+
+/**
+ * Below here is the code for the number of people input mutation
+ */
+numPeople.addEventListener('input',(event)=>{
+    num_input = event.target.value
+    if(num_input === ""){
+        
+    }else{
+        total.innerHTML = num_input
+    }
+})
+
+numPeople.onblur = function(){
+    if(num_input === ""){
+        const empty = document.querySelector(".empty-message-num")
+        let p = document.createElement('p')
+        p.innerHTML = "Can't be empty"
+        p.classList.add("cantBeNum")
+        empty.append(p)
+        people.classList.remove("input-div")
+        people.classList.add("input-empty")
+    }
+}
+
+numPeople.onfocus = function(){
+    let empty = document.querySelector(".empty-message-num")
+    let p = document.querySelector(".cantBeNum")
+    if(p !== null){
+        empty.removeChild(p)
+        people.classList.remove("input-empty")
+        people.classList.add("input-div")
     }
 }
