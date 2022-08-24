1b689cec769e620b39f2bf11f997912c7e44c3c4
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-24 10:19:50 +0300

    Included the empty message display for the empty input

--- tip-calculator-app-main/js/App.js
+++ tip-calculator-app-main/js/App.js
@@ -1,6 +1,5 @@
-console.log("I am here")
-import { removeComma } from "./removeOperator"
-import { thousandOperator } from "./thousandOperator"
+import { removeComma } from "./removeOperator.js"
+import { thousandOperator } from "./thousandOperator.js"
 
 const bill = document.getElementById("bill-amount")
 const numPeople = document.getElementById("num-people")
@@ -12,10 +11,29 @@ let bill_input = ""
 bill.addEventListener('input',(event)=>{
     bill_input = event.target.value 
     console.log(bill_input)
-    // if(bill_input === ""){
-    //     tipAmount.innerHTML = "0.00"
-    // }else{
-    //     tipAmount.innerHTML = bill_input
-    // }
-    // event.target.value = thousandOperator(removeComma(bill_input))
+    if(bill_input === ""){
+        tipAmount.innerHTML = "0.00"
+    }else{
+        tipAmount.innerHTML = bill_input
+    }
 })
+
+// Using the onblur and onfocus
+bill.onblur = function(){
+    if(bill_input === ""){
+        const empty = document.querySelector(".empty-message")
+        let p = document.createElement('p')
+        p.innerHTML = "Can't be empty"
+        p.classList.add("cantBe")
+        empty.append(p)
+    }
+}
+
+// When forcus is on, then, it will remove the 'can't be empty message'
+bill.onfocus = function(){
+    let empty = document.querySelector(".empty-message")
+    let p = document.querySelector(".cantBe")
+    if(p !== null){
+        empty.removeChild(p)
+    }
+}
