992161602ecbfa9a752e34cbd0ed55b1e82e4940
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-07 14:30:42 +0300

    added in a handler function in App class that will be used privately

--- noteApp/js/App.js
+++ noteApp/js/App.js
@@ -1,9 +1,29 @@
-import NotesView from "./NotesView";
+import NotesView from "./NotesView.js";
 
 export default class App{
     constructor(root){
         this.notes = [];
         this.activeNote = null;
-        this.view = NotesView(root);
+        this.view = NotesView(root, this._handler());
+    }
+
+    _handler(){
+        return{
+            onNoteSelect: noteId =>{
+                console.log("Note selected is "+ noteId)
+            },
+
+            onNoteAdd: ()=>{
+                console.log("Note add");
+            },
+
+            onNoteEdit:(title, body)=>{
+                console.log(title, body)
+            },
+
+            onNoteDelete: noteId => {
+                console.log("Note deleted", noteId)
+            }
+        }
     }
 }
