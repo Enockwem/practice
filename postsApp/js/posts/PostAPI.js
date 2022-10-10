9ec64f0aa3b8990e96d25b4458f934bf57f907bb
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-10 14:49:50 +0300

    Designed a function that retrieves a post from the database and shows it

--- postsApp/js/posts/PostAPI.js
+++ postsApp/js/posts/PostAPI.js
@@ -1,6 +1,8 @@
 export default class PostAPI{
     static getAllPosts(){
+        const post = JSON.parse(localStorage.getItem("posts") || "[]")
 
+        return post;
     }
 
     static savePost(post){
