ffe125ced01483034c5b50d82ea1fbccc9322b63
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-10 14:29:45 +0300

    I have created the rootView of the page

--- postsApp/js/posts/PostView.js
+++ postsApp/js/posts/PostView.js
@@ -1,3 +1,22 @@
 export default class PostView{
-    
+    constructor(root, {onPostSelecte, onColorChange}={}){
+        this.root = root;
+        this.onPostSelecte = onPostSelecte;
+        this.onColorChange = onColorChange;
+
+        this.root.innerHTML = `
+        <div class="container">
+            <div class="sidebar__container">
+                <div class="sidebar__section">Dashboard</div>
+                <div class="sidebar__section">Posts</div>
+                <div class="sidebar__section">Settings</div>
+            </div>
+            <div class="preview__section">
+                <div class="preview_titile">Title</div>
+                <div class="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit nisi assumenda, ullam libero fuga perferendis perspiciatis quia nam recusandae debitis quae unde sint ab qui voluptatem illum voluptatibus? Fuga, ad.</div>
+                <div class="recent-posts">View recent posts</div>
+            </div>
+        </div>
+        `
+    }
 }
