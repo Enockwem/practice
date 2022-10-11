d67a2aa35681f1eca1798caf8e24e7ace1641947
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-11 11:00:03 +0300

    Changed the switch for the dashboard in App.js file

--- postsApp/js/posts/App.js
+++ postsApp/js/posts/App.js
@@ -3,6 +3,29 @@ import PostView from "./PostView.js";
 export default class App{
     constructor(root){
         this.root = root
-        this.view = new PostView(this.root)
+        this.view = new PostView(this.root, this._handler())
+    }
+
+    _handler(){
+        return {
+            onPreviewChange: section=>{
+                switch(section){
+                    case "Dashboard":
+                        // window.alert("Dashboard")
+                        this.view.showDashboard()
+                        break;
+                    case "Posts":
+                        window.alert("Posts")
+                        break;
+                    case "Settings":
+                        window.alert("Settings")
+                        break;
+                    default:
+                        window.alert("Nothing selected")
+                        break;
+
+                }
+            }
+        }
     }
 }
