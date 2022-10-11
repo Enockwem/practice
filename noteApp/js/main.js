2bb02ba3104d18ae1425e61a2f2fe033c71553a7
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-11 13:36:05 +0300

    i have modified main.js

--- noteApp/js/main.js
+++ noteApp/js/main.js
@@ -1,6 +1,8 @@
 import App from './App.js'
+import NotesAPI from './NotesAPI.js'
 
-const root = document.getElementById('app')
+// const root = document.getElementById('app')
 
 
-const app = new App(root)
+// const app = new App(root)
+console.log(NotesAPI.getAllNotes())
