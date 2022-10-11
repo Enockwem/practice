5dd3b869264f3b4a9291004f005fc2187011fb23
Wasswa Enock Male <enockmale97@gmail.com>
2022-10-11 13:37:13 +0300

    i have modified index.js

--- postsApp/js/index.js
+++ postsApp/js/index.js
@@ -1,5 +1,10 @@
 import App from "./posts/App.js";
+import PostAPI from "./posts/PostAPI.js";
 
 const root = document.getElementById("app")
 
 const app  = new App(root)
+
+// const title = "Working";
+// const body = "Working as a software developer is something I am looking forward to."
+// PostAPI.savePost(title, body)
