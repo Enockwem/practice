8bd8b1d9865ab4f4f63e487d46b19b521fb1e749
Wasswa Enock Male <enockmale97@gmail.com>
2022-11-07 16:07:53 +0300

    I have made the index.js file clear

--- randomQuoteGenerator/index.js
+++ randomQuoteGenerator/index.js
@@ -1,4 +1,7 @@
 import App from "./js/App.js"
+import QuotesAPI from "./js/QuotesAPI.js";
 
 const root = document.getElementById("root");
 const app = new App(root)
+// const quote = new QuotesAPI();
+console.log(QuotesAPI.getQuotes())
--- randomQuoteGenerator/js/QuotesAPI.js
+++ randomQuoteGenerator/js/QuotesAPI.js
@@ -2,7 +2,10 @@ export default class QuotesAPI{
     static getQuotes(){
         return fetch("https://type.fit/api/quotes")
         .then((response)=>{
+            console.log(response.status)
             return response.json()
+        }).then((data)=>{
+            console.log(data)
         })
     }
 }
