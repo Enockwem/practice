0a7eb9caef21a8253e2450710efef0dae8d7710a
wasswaenockmale <enockmale97@gmail.com>
2023-05-13 11:28:19 +0300

    generator.js function

---
+++ jsPractice/Generator.js
@@ -0,0 +1,31 @@
+function* powers(n){
+    for(let current = n;; current *= n){
+        yield current;
+    }
+}
+
+for(let power of powers(3)){
+    if(power > 50) break;
+    console.log(power);
+}
+
+async function findInStorage(nest, name){
+    let local = await storage(nest, name);
+    if(local != null) return local;
+
+    let sources = network(nest).filter(n => n != nest.name);
+
+}
+
+// I need to just try something
+// try{
+//     setTimeout(()=>{
+//         throw new Error("Woosh");
+//     }, 20);
+// }catch(_){
+//     // This will not run
+//     console.log("Caught!");
+// }
+
+let fifteen = Promise.resolve(15);
+fifteen.then(value => console.log(`Got  ${value}`));
