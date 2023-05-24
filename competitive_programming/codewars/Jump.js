e5a328743bbad77d04f0061ebc5dfea539b0eacc
wasswaenockmale <enockmale97@gmail.com>
2023-05-24 19:40:27 +0300

    I have to check whether it leads to a dead end

--- competitive_programming/codewars/Jump.js
+++ competitive_programming/codewars/Jump.js
@@ -1,4 +1,12 @@
-// Okay what if the path suggested is doesn't let to the winnable path 
+// Okay what if the path suggested is doesn't let to the winnable path
+// At each step you can jump ahead in the array 
+// as far as the integer at the current cell, 
+// or any smaller number of cells. You win if 
+// there is a path that allows you to jump from 
+// one cell to another, eventually jumping past 
+// the end of the array, otherwise you lose.
+
+
 function canJump(array){
   let curr = array[0]
     if(array.length === 1){
